646108a4711b751c4c15322f599ce6e293989e13
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2023-03-12 14:57:15 +0000

    Update __init__.py

--- custom_components/idrac_power_monitor/__init__.py
+++ custom_components/idrac_power_monitor/__init__.py
@@ -1,29 +1,37 @@
-"""iDrac power usage monitor"""
-
+# Import necessary functions and classes from Home Assistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+# Import constants used in this module from the const.py file
 from .const import DOMAIN, DATA_IDRAC_REST_CLIENT, HOST, USERNAME, PASSWORD
+
+# Import the IdracRest class from the idrac_rest.py file
 from .idrac_rest import IdracRest
 
+# Define the list of platforms used in this integration (in this case, just the "sensor" platform)
 PLATFORMS: list[str] = ["sensor"]
 
-
+# Define the async_setup_entry function, which sets up the iDrac connection from a config entry
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up the iDrac connection from a config entry."""
+    # Add the iDracRest object to hass.data using the entry_id as a key
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
         DATA_IDRAC_REST_CLIENT: IdracRest(entry.data[HOST], entry.data[USERNAME], entry.data[PASSWORD])
     }
 
+    # Set up the platforms associated with this integration using the config entry
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
+    # Return True to indicate that the setup was successful
     return True
 
-
+# Define the async_unload_entry function, which unloads the iDrac config entry and associated platforms
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
+    # Unload the platforms associated with this integration using the config entry
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    # If the platforms were successfully unloaded, remove the iDracRest object from hass.data
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
+    # Return True if everything was successfully unloaded, False otherwise
     return unload_ok
