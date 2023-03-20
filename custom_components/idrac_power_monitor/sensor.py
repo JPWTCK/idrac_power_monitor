cc64552d6a70cc7735707d69bd49fa789d7fb5fd
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2023-03-20 16:23:07 +0000

    revert sensor.py

--- custom_components/idrac_power_monitor/sensor.py
+++ custom_components/idrac_power_monitor/sensor.py
@@ -1,70 +1,79 @@
-from __future__ import annotations
-import logging
-from datetime import datetime
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
-from requests import RequestException
-from .const import DOMAIN, CURRENT_POWER_SENSOR_DESCRIPTION, DATA_IDRAC_REST_CLIENT, JSON_NAME, JSON_MODEL, JSON_MANUFACTURER, JSON_SERIAL_NUMBER, TOTAL_POWER_SENSOR_DESCRIPTION
-from .idrac_rest import IdracRest
+import requests
+from homeassistant.exceptions import HomeAssistantError
 
-_LOGGER = logging.getLogger(__name__)
+from .const import (
+    JSON_NAME, JSON_MANUFACTURER, JSON_MODEL, JSON_SERIAL_NUMBER,
+    JSON_POWER_CONSUMED_WATTS, JSON_FIRMWARE_VERSION
+)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    rest_client = hass.data[DOMAIN][entry.entry_id][DATA_IDRAC_REST_CLIENT]
-    info = await hass.async_add_executor_job(rest_client.get_device_info)
-    firmware_version = await hass.async_add_executor_job(rest_client.get_firmware_version)
-    device_info = DeviceInfo(
-        identifiers=[('domain', DOMAIN), ('model', info[JSON_MODEL]), ('serial', info[JSON_SERIAL_NUMBER])],
-        name=info[JSON_NAME],
-        manufacturer=info[JSON_MANUFACTURER],
-        model=info[JSON_MODEL],
-        sw_version=firmware_version
-    )
-    async_add_entities([
-        IdracCurrentPowerSensor(rest_client, device_info, f"{info[JSON_SERIAL_NUMBER]}_{info[JSON_MODEL]}_current"),
-        IdracTotalPowerSensor(rest_client, device_info, f"{info[JSON_SERIAL_NUMBER]}_{info[JSON_MODEL]}_total")
-    ])
+# Define some constants for the iDRAC REST API paths
+protocol = 'https://'
+drac_managers_path = '/redfish/v1/Managers/iDRAC.Embedded.1'
+drac_chassis_path = '/redfish/v1/Chassis/System.Embedded.1'
+drac_powercontrol_path = '/redfish/v1/Chassis/System.Embedded.1/Power/PowerControl'
 
-class IdracCurrentPowerSensor(SensorEntity):
-    def __init__(self, rest: IdracRest, device_info, unique_id):
-        self.rest = rest
-        self._attr_device_info = device_info
-        self._attr_unique_id = unique_id
-        self.entity_description = CURRENT_POWER_SENSOR_DESCRIPTION
-        self._attr_native_value = None
+# Define a function to handle HTTP errors returned by the iDRAC REST API
+def handle_error(result):
+    if result.status_code == 401:
+        raise InvalidAuth()
 
-    def update(self) -> None:
-        self._attr_native_value = self.rest.get_power_usage()
+    if result.status_code == 404:
+        error = result.json()['error']
+        if error['code'] == 'Base.1.0.GeneralError' and 'RedFish attribute is disabled' in \
+                error['@Message.ExtendedInfo'][0]['Message']:
+            raise RedfishConfig()
 
-class IdracTotalPowerSensor(SensorEntity):
-    def __init__(self, rest: IdracRest, device_info, unique_id):
-        self.rest = rest
-        self.entity_description = TOTAL_POWER_SENSOR_DESCRIPTION
-        self._attr_device_info = device_info
-        self._attr_unique_id = unique_id
-        self._attr_native_value = 0.0
-        self.last_power_usage = None
-        self.last_update_time = None
+    if result.status_code != 200:
+        raise CannotConnect(result.text)
 
-    def update(self) -> None:
-        now = datetime.now()
-        if self.last_power_usage is None:
-            # This is the first reading
-            self.last_power_usage = self.rest.get_power_usage()
-            self.last_update_time = now
-        else:
-            # Calculate the energy usage using trapezoidal rule integration
-            delta_time = (now - self.last_update_time).total_seconds()
-            if delta_time >= 60:
-                # Calculate the average power usage over the current interval
-                avg_power_usage = (self.rest.get_power_usage() + self.last_power_usage) / 2.0
+# Define a class to interact with the iDRAC REST API
+class IdracRest:
+    def __init__(self, host, username, password):
+        self.host = host
+        self.auth = (username, password)
 
-                # Calculate the energy usage using the trapezoidal rule
-                energy_usage = avg_power_usage * delta_time / 3600.0  # kWh
-                self._attr_native_value += energy_usage
+    # Define a method to get the power usage from the iDRAC REST API
+    def get_power_usage(self):
+        result = self.get_path(drac_powercontrol_path)
+        handle_error(result)
 
-                # Update the last power usage and update time
-                self.last_power_usage = self.rest.get_power_usage()
-                self.last_update_time = now
+        power_results = result.json()
+        return power_results[JSON_POWER_CONSUMED_WATTS]
+
+    # Define a method to get device info from the iDRAC REST API
+    def get_device_info(self):
+        result = self.get_path(drac_chassis_path)
+        handle_error(result)
+
+        chassis_results = result.json()
+        return {
+            JSON_NAME: chassis_results[JSON_NAME],
+            JSON_MANUFACTURER: chassis_results[JSON_MANUFACTURER],
+            JSON_MODEL: chassis_results[JSON_MODEL],
+            JSON_SERIAL_NUMBER: chassis_results[JSON_SERIAL_NUMBER]
+        }
+
+    # Define a method to get the firmware version from the iDRAC REST API
+    def get_firmware_version(self):
+        result = self.get_path(drac_managers_path)
+        handle_error(result)
+
+        manager_results = result.json()
+        return manager_results[JSON_FIRMWARE_VERSION]
+
+    # Define a method to get a path from the iDRAC REST API
+    def get_path(self, path):
+        return requests.get(protocol + self.host + path, auth=self.auth, verify=False)
+
+
+# Define some custom exceptions for error handling
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
+
+
+class RedfishConfig(HomeAssistantError):
+    """Error to indicate that Redfish was not properly configured"""
