ae5a80e136067a71e703239f5be70caf8eb00052
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2023-03-22 16:18:40 +0000

    new baseline of ssl

--- custom_components/idrac_power_monitor/idrac_rest.py
+++ custom_components/idrac_power_monitor/idrac_rest.py
@@ -1,12 +1,7 @@
-import requests
-import ssl
-import socket
+import os
 import tempfile
-from contextlib import closing
+import requests
 from homeassistant.exceptions import HomeAssistantError
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.connection import VerifiedHTTPSConnection
-from requests.packages.urllib3.connectionpool import HTTPSConnectionPool
 
 from .const import (
     JSON_NAME, JSON_MANUFACTURER, JSON_MODEL, JSON_SERIAL_NUMBER,
@@ -33,67 +28,11 @@ def handle_error(result):
     if result.status_code != 200:
         raise CannotConnect(result.text)
 
-# Create a custom requests adapter that ignores the hostname mismatch error
-class HostNameIgnoringVerifiedHTTPSConnection(VerifiedHTTPSConnection):
-    def _match_hostname(self, cert, asserted_hostname):
-        return True
-
-class HostNameIgnoringAdapter(HTTPAdapter):
-    def __init__(self, host, port):
-        super().__init__()
-        self.host = host
-        self.port = port
-        self.poolmanager = None
-
-    def init_poolmanager(self, connections, maxsize, block=False, **pool_kwargs):
-        self.poolmanager = HTTPSConnectionPool(
-            host=self.host,
-            port=self.port,
-            cert_reqs="CERT_NONE",
-            ssl_version=ssl.PROTOCOL_TLS,
-            connection_class=HostNameIgnoringVerifiedHTTPSConnection,
-            **pool_kwargs
-        )
-
 # Define a class to interact with the iDRAC REST API
 class IdracRest:
     def __init__(self, host, username, password):
         self.host = host
         self.auth = (username, password)
-        self.cert_file = self.download_self_signed_cert()
-
-    # Define a method to download the self-signed certificate and save it to a temporary file
-    def download_self_signed_cert(self):
-        # Connect to the iDRAC host with SSL
-        with closing(socket.create_connection((self.host, 443))) as sock:
-            context = ssl.create_default_context()
-            context.check_hostname = False
-            context.verify_mode = ssl.CERT_NONE
-
-            with closing(context.wrap_socket(sock, server_hostname=self.host)) as ssl_sock:
-                # Get the iDRAC certificate
-                cert_pem = ssl.DER_cert_to_PEM_cert(ssl_sock.getpeercert(binary_form=True))
-
-        # Save the certificate to a temporary file
-        cert_file = tempfile.NamedTemporaryFile(delete=False)
-        cert_file.write(cert_pem.encode())
-        cert_file.close()
-
-        return cert_file.name
-
-    # Define a method to get a path from the iDRAC REST API
-    def get_path(self, path):
-        session = requests.Session()
-        session.verify = self.cert_file
-        session.auth = self.auth
-        adapter = HostNameIgnoringAdapter(self.host, 443)  # Instantiate the custom adapter
-        session.mount(protocol + self.host, adapter)  # Use the custom adapter
-        pool_kwargs = {'host': self.host}  # Pass the host parameter to the init_poolmanager method
-        adapter.init_poolmanager(pool_connections=1, pool_maxsize=1, pool_block=True, **pool_kwargs)
-        return session.get(protocol + self.host + path)
-
-
-    # Other methods in IdracRest class
 
     # Define a method to get the power usage from the iDRAC REST API
     def get_power_usage(self):
@@ -124,12 +63,31 @@ class IdracRest:
         manager_results = result.json()
         return manager_results[JSON_FIRMWARE_VERSION]
 
+    # Define a method to get a path from the iDRAC REST API
+    def get_path(self, path):
+        # Load the certificate temporarily
+        with tempfile.NamedTemporaryFile(delete=False) as cert_file:
+            cert_file.write(b"-----BEGIN CERTIFICATE-----\n")
+            cert_file.write(b"YOUR_CERTIFICATE_CONTENT_HERE")
+            cert_file.write(b"\n-----END CERTIFICATE-----\n")
+            cert_file.flush()
+
+            # Pass the certificate to the verify parameter
+            response = requests.get(protocol + self.host + path, auth=self.auth, verify=cert_file.name)
+
+            # Remove the temporary certificate file
+            os.unlink(cert_file.name)
+
+        return response
+
 # Define some custom exceptions for error handling
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
 
+
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
 
 class RedfishConfig(HomeAssistantError):
     """Error to indicate that Redfish was not properly configured"""
