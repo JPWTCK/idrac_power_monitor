4ba48886d9935b6c2120556b3d896d3d9e00f38e
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2023-03-23 16:11:40 +0000

    Update idrac_rest.py

--- custom_components/idrac_power_monitor/idrac_rest.py
+++ custom_components/idrac_power_monitor/idrac_rest.py
@@ -1,12 +1,11 @@
-"""
-This module defines a class IdracRest and some helper functions and classes to interact with the Dell iDRAC REST API.
-"""
-
-import ssl
-from typing import Dict
 import requests
+import ssl
+import requests.packages.urllib3.util.ssl_ as ssl_
+ssl_.DEFAULT_CIPHERS = ssl_.DEFAULT_CIPHERS + ':HIGH:!DH:!aNULL'
+from homeassistant.exceptions import HomeAssistantError
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
 
 from .const import (
     JSON_NAME, JSON_MANUFACTURER, JSON_MODEL, JSON_SERIAL_NUMBER,
@@ -14,15 +13,13 @@ from .const import (
 )
 
 # Define some constants for the iDRAC REST API paths
-PROTOCOL = 'https://'
-DRAC_MANAGERS_PATH = '/redfish/v1/Managers/iDRAC.Embedded.1'
-DRAC_CHASSIS_PATH = '/redfish/v1/Chassis/System.Embedded.1'
-DRAC_POWERCONTROL_PATH = '/redfish/v1/Chassis/System.Embedded.1/Power/PowerControl'
-
+protocol = 'https://'
+drac_managers_path = '/redfish/v1/Managers/iDRAC.Embedded.1'
+drac_chassis_path = '/redfish/v1/Chassis/System.Embedded.1'
+drac_powercontrol_path = '/redfish/v1/Chassis/System.Embedded.1/Power/PowerControl'
 
 # Define a function to handle HTTP errors returned by the iDRAC REST API
 def handle_error(result):
-    """Handle HTTP errors returned by the iDRAC REST API."""
     if result.status_code == 401:
         raise InvalidAuth()
 
@@ -35,21 +32,17 @@ def handle_error(result):
     if result.status_code != 200:
         raise CannotConnect(result.text)
 
-
 # Define a custom SSL context adapter for requests
 class CustomSSLAdapter(HTTPAdapter):
-    """A custom SSL context adapter for requests."""
-
-    def __init__(self, ssl_options: str = ssl.DEFAULT_CIPHERS, *args, **kwargs):
+    def __init__(self, ssl_options=ssl_.DEFAULT_CIPHERS, *args, **kwargs):
         self.ssl_options = ssl_options
         super().__init__(*args, **kwargs)
 
     def init_poolmanager(self, *args, **kwargs):
-        """Initialize the pool manager with a custom SSL context."""
         context = ssl.create_default_context()
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
-        self.poolmanager = PoolManager(
+        self.poolmanager = CustomPoolManager(
             assert_hostname=False,
             cert_reqs=ssl.CERT_NONE,
             ssl_options=self.ssl_options,
@@ -57,34 +50,26 @@ class CustomSSLAdapter(HTTPAdapter):
             **kwargs
         )
 
-
 # Define a class to interact with the iDRAC REST API
 class IdracRest:
-    """A class to interact with the iDRAC REST API."""
-
-    def __init__(self, host: str, username: str, password: str):
+    def __init__(self, host, username, password):
         self.host = host
         self.auth = (username, password)
         self.session = requests.Session()
         self.session.verify = False
         requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
-        ssl_adapter = CustomSSLAdapter()
-        self.session.mount(PROTOCOL, ssl_adapter)
-
     # Define a method to get the power usage from the iDRAC REST API
-    def get_power_usage(self) -> float:
-        """Get the power usage from the iDRAC REST API."""
-        result = self.get_path(DRAC_POWERCONTROL_PATH)
+    def get_power_usage(self):
+        result = self.get_path(drac_powercontrol_path)
         handle_error(result)
 
         power_results = result.json()
         return power_results[JSON_POWER_CONSUMED_WATTS]
 
-# Define a method to get device info from the iDRAC REST API
-    def get_device_info(self) -> Dict[str, str]:
-        """Get device information from the iDRAC REST API."""
-        result = self.get_path(DRAC_CHASSIS_PATH)
+    # Define a method to get device info from the iDRAC REST API
+    def get_device_info(self):
+        result = self.get_path(drac_chassis_path)
         handle_error(result)
 
         chassis_results = result.json()
@@ -96,28 +81,23 @@ class IdracRest:
         }
 
     # Define a method to get the firmware version from the iDRAC REST API
-    def get_firmware_version(self) -> str:
-        """Get the firmware version from the iDRAC REST API."""
-        result = self.get_path(DRAC_MANAGERS_PATH)
+    def get_firmware_version(self):
+        result = self.get_path(drac_managers_path)
         handle_error(result)
 
         manager_results = result.json()
         return manager_results[JSON_FIRMWARE_VERSION]
-
+    
     # Define a method to get a path from the iDRAC REST API
-    def get_path(self, path: str) -> requests.Response:
-        """Get a path from the iDRAC REST API."""
-        return self.session.get(f"{PROTOCOL}{self.host}{path}", auth=self.auth)
-
+    def get_path(self, path):
+        return self.session.get(protocol + self.host + path, auth=self.auth)
 
 # Define some custom exceptions for error handling
-class CannotConnect(Exception):
+class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
 
-
-class InvalidAuth(Exception):
+class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
 
-
-class RedfishConfig(Exception):
+class RedfishConfig(HomeAssistantError):
     """Error to indicate that Redfish was not properly configured"""
