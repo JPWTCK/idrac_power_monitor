54390a47911dbd253722e84550dfb630955c3781
JPWTCK <39982096+JPWTCK@users.noreply.github.com>
2023-03-23 15:16:51 +0000

    fixes

--- custom_components/idrac_power_monitor/config_flow.py
+++ custom_components/idrac_power_monitor/config_flow.py
@@ -41,6 +41,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
 
         errors = {}
+        info = None
 
         try:
             info = await self.validate_input(user_input)
@@ -53,15 +54,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         except Exception as ex:
             _LOGGER.exception("Unexpected exception: %s", ex)
             errors["base"] = "unknown"
-        finally:
-            if not errors:
-                return self.async_create_entry(title=info["model_name"], data=user_input)
 
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA,
-                errors=errors
-            )
+        if not errors and info:
+            return self.async_create_entry(title=info["model_name"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=STEP_USER_DATA_SCHEMA,
+            errors=errors
+        )
 
     async def validate_input(self, data: dict[str, Any]) -> dict[str, Any]:
         """
